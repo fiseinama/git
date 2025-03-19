6f7564207bc4957132b72e31369b385f131ceb95
Alina <alinad1103@gmail.com>
2025-03-19 16:12:49 +0400

    Добавляем функцию fib(n), которая вычисляет n-ое число Фибоначчи для последовательности (1, 1, ...). Добавляем переменную n (искомый элемент последовательности), значение которой вводится пользователем. Также появляется проверка на отрицательный элемент, после чего результат вычислений записываем в переменную res и выводим.

--- git/git.cpp
+++ git/git.cpp
@@ -1,7 +1,20 @@
 #include<iostream>
 using namespace std;
 
+int fib(int n) {
+	if (n == 1 || n == 2)
+		return 1;
+	return fib(n - 1) + fib(n - 2);
+}
+
 int main() {
 	cout << "Hello, World!" << endl;
+	int n;
+	cout << "n = ";
+	cin >> n;
+	if (n < 0)
+		return 0;
+	int res = fib(n);
+	cout << "fib(" << n << ") = " << res << endl;
 	return 0;
 }
